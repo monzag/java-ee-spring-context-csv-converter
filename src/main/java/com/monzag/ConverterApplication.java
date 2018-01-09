48756aa07dbbe7ba6cfe49d9c98b3b7a310af300
monzag <zaganiacz.m@gmail.com>
2018-01-09 19:04:42 +0100

    Add try/catch block to main

--- src/main/java/com/monzag/ConverterApplication.java
+++ src/main/java/com/monzag/ConverterApplication.java
@@ -1,7 +1,20 @@
 package com.monzag;
 
+import com.monzag.exceptions.FileNotFoundException;
+
 public class ConverterApplication
 {
     public static void main( String[] args ) {
+        try {
+            if (args.length > 0) {
+                System.out.println("ho ho");
+
+            } else {
+                throw new FileNotFoundException();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
