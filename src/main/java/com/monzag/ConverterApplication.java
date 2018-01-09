b2546228d3324365c06db0898b284bf8d4d3442c
monzag <zaganiacz.m@gmail.com>
2018-01-09 23:10:09 +0100

    Add options to try block

--- src/main/java/com/monzag/ConverterApplication.java
+++ src/main/java/com/monzag/ConverterApplication.java
@@ -1,13 +1,25 @@
 package com.monzag;
 
+import com.monzag.controllers.SimpleCsvConverter;
 import com.monzag.exceptions.FileNotFoundException;
+import com.monzag.models.FileType;
+
+import java.io.File;
 
 public class ConverterApplication
 {
     public static void main( String[] args ) {
+        SimpleCsvConverter csvConverter = new SimpleCsvConverter();
+
         try {
-            if (args.length > 0) {
-                System.out.println("ho ho");
+            if (args.length == 1) {
+                File file = new File(args[0]);
+                csvConverter.convert(file);
+
+            } else if (args.length == 2) {
+                FileType type = FileType.valueOf(args[0].toUpperCase());
+                File file = new File(args[1]);
+                csvConverter.convert(file, type);
 
             } else {
                 throw new FileNotFoundException();
@@ -15,6 +27,9 @@ public class ConverterApplication
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("incorrect type");
         }
     }
 }
