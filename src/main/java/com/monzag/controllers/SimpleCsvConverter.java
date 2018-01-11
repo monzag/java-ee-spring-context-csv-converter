061335ad14b0f177acc6c7dce383b95417d8fe01
monzag <zaganiacz.m@gmail.com>
2018-01-11 20:07:19 +0100

    Add constructor and field FileReader

--- src/main/java/com/monzag/controllers/SimpleCsvConverter.java
+++ src/main/java/com/monzag/controllers/SimpleCsvConverter.java
@@ -1,10 +1,17 @@
 package com.monzag.controllers;
 
 import com.monzag.models.FileType;
+import com.monzag.service.FileReader;
 
 import java.io.File;
 
 public class SimpleCsvConverter {
+
+    private FileReader reader;
+
+    public SimpleCsvConverter(FileReader reader) {
+        this.reader = reader;
+    }
 
     public void convert(File file) {
         System.out.println("convert from csv file");
