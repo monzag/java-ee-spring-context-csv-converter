dc13add2f1540790ad76c70c46782f0011fa91c8
monzag <zaganiacz.m@gmail.com>
2018-01-11 21:59:21 +0100

    Change File object to Path

--- src/main/java/com/monzag/ConverterApplication.java
+++ src/main/java/com/monzag/ConverterApplication.java
@@ -1,37 +1,45 @@
 package com.monzag;
 
 import com.monzag.controllers.SimpleCsvConverter;
-import com.monzag.exceptions.FileNotFoundException;
+import com.monzag.exceptions.InputNotDefinedException;
 import com.monzag.models.FileType;
 import com.monzag.service.FileReader;
+import com.monzag.service.OutputFormatterFactory;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ConverterApplication
 {
     public static void main( String[] args ) {
+        Path path;
         FileReader reader = new FileReader();
-        SimpleCsvConverter csvConverter = new SimpleCsvConverter(reader);
+        OutputFormatterFactory offactory = new OutputFormatterFactory();
+        SimpleCsvConverter csvConverter = new SimpleCsvConverter(reader, offactory);
 
         try {
             if (args.length == 1) {
-                File file = new File(args[0]);
-                csvConverter.convert(file);
+                path = Paths.get(args[0]).toRealPath();
+                csvConverter.convert(path);
 
             } else if (args.length == 2) {
                 FileType type = FileType.valueOf(args[0].toUpperCase());
-                File file = new File(args[1]);
-                csvConverter.convert(file, type);
+                path = Paths.get(args[1]).toRealPath();
+                csvConverter.convert(path, type);
 
             } else {
-                throw new FileNotFoundException();
+                throw new InputNotDefinedException();
             }
 
-        } catch (FileNotFoundException e) {
+        } catch (InputNotDefinedException e) {
             System.out.println(e.getMessage());
 
         } catch (IllegalArgumentException e) {
             System.out.println("incorrect type");
+
+        } catch (IOException e) {
+            System.out.println("File not exist");
         }
     }
 }
