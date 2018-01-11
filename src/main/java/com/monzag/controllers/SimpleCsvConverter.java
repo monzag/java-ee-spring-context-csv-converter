ed62c45b93a9bb5cbb1dcb5d905c5a6f7e86fbf4
monzag <zaganiacz.m@gmail.com>
2018-01-11 21:09:35 +0100

    Add OutputFormatterFactory to SimpleCsvConverter

--- src/main/java/com/monzag/controllers/SimpleCsvConverter.java
+++ src/main/java/com/monzag/controllers/SimpleCsvConverter.java
@@ -2,22 +2,28 @@ package com.monzag.controllers;
 
 import com.monzag.models.FileType;
 import com.monzag.service.FileReader;
+import com.monzag.service.OutputFormatter;
+import com.monzag.service.OutputFormatterFactory;
 
 import java.io.File;
 
 public class SimpleCsvConverter {
 
     private FileReader reader;
+    private OutputFormatterFactory outputFormatterFactory;
 
-    public SimpleCsvConverter(FileReader reader) {
+    public SimpleCsvConverter(FileReader reader, OutputFormatterFactory outputFormatterFactory) {
         this.reader = reader;
+        this.outputFormatterFactory = outputFormatterFactory;
     }
 
     public void convert(File file) {
+        OutputFormatter outputFormatter = outputFormatterFactory.createByFormat();
         System.out.println("convert from csv file");
     }
 
     public void convert(File file, FileType type) {
+        OutputFormatter outputFormatter = outputFormatterFactory.createByFormat(type);
         System.out.println("convert from file and type");
     }
 }
