4a4a5b06fd7ada2d1a5adeed4b5c00365b231619
monzag <zaganiacz.m@gmail.com>
2018-01-11 20:13:56 +0100

    Create TableOutputFormatter

---
+++ src/main/java/com/monzag/service/TableOutputFormatter.java
@@ -0,0 +1,8 @@
+package com.monzag.service;
+
+public class TableOutputFormatter implements OutputFormatter {
+
+    public void printToConsole(String data) {
+        System.out.println("print data from csv");
+    }
+}
