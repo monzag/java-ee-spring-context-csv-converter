c75ef8fd9e7cf607c7ade631456a2790097c9658
monzag <zaganiacz.m@gmail.com>
2018-01-11 20:16:26 +0100

    Create XmlOutputFormatter

---
+++ src/main/java/com/monzag/service/XmlOutputFormatter.java
@@ -0,0 +1,8 @@
+package com.monzag.service;
+
+public class XmlOutputFormatter implements OutputFormatter {
+
+    public void printToConsole(String data) {
+        System.out.println("Print data from xml");
+    }
+}
