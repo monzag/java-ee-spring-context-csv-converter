b12720eadc84a2cb3ef8f9a068a150703131b10e
monzag <zaganiacz.m@gmail.com>
2018-01-12 04:27:23 +0100

    Add printToConsole as xml type

--- src/main/java/com/monzag/service/XmlOutputFormatter.java
+++ src/main/java/com/monzag/service/XmlOutputFormatter.java
@@ -1,8 +1,26 @@
 package com.monzag.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+
 public class XmlOutputFormatter implements OutputFormatter {
 
-    public void printToConsole(String data) {
-        System.out.println("Print data from xml");
+    public void printToConsole(InputStream inputStream) throws IOException {
+        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+        CsvMapper mapper = new CsvMapper();
+        List<Object> readAll = mapper.readerFor(Map.class).with(schema).readValues(inputStream).readAll();
+
+        ObjectMapper xmlMapper = new XmlMapper();
+        for (Object object : readAll) {
+            System.out.println(xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object));
+        }
     }
 }
