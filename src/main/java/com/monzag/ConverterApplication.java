957ee83c462f8e7789a5bbcbcbd49997a18b8ed3
monzag <zaganiacz.m@gmail.com>
2018-01-12 14:55:55 +0100

    Refactor magic numbers

--- src/main/java/com/monzag/ConverterApplication.java
+++ src/main/java/com/monzag/ConverterApplication.java
@@ -9,26 +9,32 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ConverterApplication
-{
+public class ConverterApplication {
+
+    private static final int ONE_ITEM = 1;
+    private static final int TWO_ITEMS = 2;
+    private static final int FIRST_INDEX = 0;
+    private static final int SECOND_INDEX = 1;
+
     public static void main( String[] args ) {
-        Path path;
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         SimpleCsvConverter csvConverter = (SimpleCsvConverter) ctx.getBean("simpleCsvConverter");
+        Path path;
+        FileType type = null;
 
         try {
-            if (args.length == 1) {
-                path = Paths.get(args[0]).toRealPath();
-                csvConverter.convert(path);
+            if (args.length == ONE_ITEM) {
+                path = Paths.get(args[FIRST_INDEX]).toRealPath();
 
-            } else if (args.length == 2) {
-                FileType type = FileType.valueOf(args[0].toUpperCase());
-                path = Paths.get(args[1]).toRealPath();
-                csvConverter.convert(path, type);
+            } else if (args.length == TWO_ITEMS) {
+                type = FileType.valueOf(args[FIRST_INDEX].toUpperCase());
+                path = Paths.get(args[SECOND_INDEX]).toRealPath();
 
             } else {
                 throw new InputNotDefinedException();
             }
+
+            csvConverter.convert(path, type);
 
         } catch (InputNotDefinedException e) {
             System.out.println(e.getMessage());
