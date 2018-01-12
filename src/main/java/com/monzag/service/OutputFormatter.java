6420c6908f2e26e518388b8891a54b6ee0b0ebad
monzag <zaganiacz.m@gmail.com>
2018-01-12 04:27:50 +0100

    Add throw exception

--- src/main/java/com/monzag/service/OutputFormatter.java
+++ src/main/java/com/monzag/service/OutputFormatter.java
@@ -1,8 +1,9 @@
 package com.monzag.service;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public interface OutputFormatter {
 
-    void printToConsole(InputStream inputStream);
+    void printToConsole(InputStream inputStream) throws IOException ;
 }
