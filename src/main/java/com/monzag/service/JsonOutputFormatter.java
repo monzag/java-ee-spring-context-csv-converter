1ec550201cbff3d04243abd63698c476242c7fa0
monzag <zaganiacz.m@gmail.com>
2018-01-12 04:25:31 +0100

    Add printToConsole as json type

--- src/main/java/com/monzag/service/JsonOutputFormatter.java
+++ src/main/java/com/monzag/service/JsonOutputFormatter.java
@@ -1,8 +1,22 @@
 package com.monzag.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
 public class JsonOutputFormatter implements OutputFormatter {
 
-    public void printToConsole(String data) {
-        System.out.println("print data from json");
+    public void printToConsole(InputStream inputStream) throws IOException {
+        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+        CsvMapper mapper = new CsvMapper();
+        List<Object> readAll = mapper.readerFor(Map.class).with(schema).readValues(inputStream).readAll();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
     }
 }
