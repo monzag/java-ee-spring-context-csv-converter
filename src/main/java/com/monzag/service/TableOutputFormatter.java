b00a60ae96674d3c964b17b754d42f46eb163bd0
monzag <zaganiacz.m@gmail.com>
2018-01-14 22:05:34 +0100

    Refactor printToConsole - add TextTable

--- src/main/java/com/monzag/service/TableOutputFormatter.java
+++ src/main/java/com/monzag/service/TableOutputFormatter.java
@@ -1,29 +1,34 @@
 package com.monzag.service;
 
-import java.io.BufferedReader;
+import com.opencsv.CSVReader;
+import dnl.utils.text.table.TextTable;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class TableOutputFormatter implements OutputFormatter {
 
-    public void printToConsole(InputStream inputStream) {
-        BufferedReader br;
-        String line;
-        StringBuilder sb;
+    private static final int COLUMN_NAMES_INDEX = 0;
 
-        try {
-            br = new BufferedReader(new InputStreamReader(inputStream));
-            sb = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
+    public void printToConsole(InputStream inputStream) throws IOException {
+        CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream));
+        List records = csvReader.readAll();
+        String[] columnNames = (String[]) records.get(COLUMN_NAMES_INDEX);
+        Object[][] data = new Object[records.size()-1][columnNames.length];
+
+        for (int i = 0; i < records.size(); i++) {
+            int columnNumber = 0;
+            if (i > 0) {
+                for (String cellValue : (String[]) records.get(i)) {
+                    data[i-1][columnNumber] = cellValue;
+                    columnNumber++;
+                }
             }
-
-            System.out.println(sb.toString());
-            br.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        TextTable table = new TextTable(columnNames, data);
+        table.printTable();
     }
 }
