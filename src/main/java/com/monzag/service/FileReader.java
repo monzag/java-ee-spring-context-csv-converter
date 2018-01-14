627ac9ff2d8f87cd3a75ea814a9f6f876085be8e
monzag <zaganiacz.m@gmail.com>
2018-01-14 22:23:59 +0100

    Add Component annotation

--- src/main/java/com/monzag/service/FileReader.java
+++ src/main/java/com/monzag/service/FileReader.java
@@ -1,10 +1,13 @@
 package com.monzag.service;
 
+import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+@Component
 public class FileReader {
 
     public InputStream readData(File file) throws FileNotFoundException{
